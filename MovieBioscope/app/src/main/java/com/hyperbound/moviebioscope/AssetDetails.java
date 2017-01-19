43dd28d4f3ecb31760ffe54cca7e79a7cef59b79
Aron <aron.raj35@gmail.com>
2017-01-19 16:03:54 +0530

    JsonJackson

---
+++ MovieBioscope/app/src/main/java/com/hyperbound/moviebioscope/AssetDetails.java
@@ -0,0 +1,36 @@
+package com.hyperbound.moviebioscope;
+
+/**
+ * Created by aarokiax on 1/19/2017.
+ */
+
+public class AssetDetails {
+
+    int assetID;
+    String url;
+    String type;
+
+    public int getAssetID() {
+        return assetID;
+    }
+
+    public void setAssetID(int assetID) {
+        this.assetID = assetID;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- MovieBioscope/app/src/main/java/com/hyperbound/moviebioscope/MainActivity.java
+++ MovieBioscope/app/src/main/java/com/hyperbound/moviebioscope/MainActivity.java
@@ -4,11 +4,17 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -78,6 +86,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        createJson();
+    }
 
+    private void createJson(){
+        Firebasedata data=createData();
+        ObjectMapper ob=new ObjectMapper();
+        StringWriter stringEmp = new StringWriter();
+        try {
+            ob.writeValue(stringEmp, data);
+            Log.i("TEST","Data : "+stringEmp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        parseJson(stringEmp.toString());
+    }
+
+    private void parseJson(String jsonData){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //convert json string to object
+        try {
+            Firebasedata data = objectMapper.readValue(jsonData, Firebasedata.class);
+            Log.i("TEST","Data : "+data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Firebasedata createData(){
+        AssetDetails assets=new AssetDetails();
+        assets.setAssetID(101);
+        assets.setType("Movie");
+        assets.setUrl("www.google.com");
+        Firebasedata data=new Firebasedata();
+        data.setCommand("Download");
+        data.setAssetDetails(assets);
+        return data;
     }
 }

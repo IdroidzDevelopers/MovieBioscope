fe87a360b0cf020851806b65753675da33f49ef1
Aron <aron.raj35@gmail.com>
2017-01-19 22:25:09 +0530

    layout design for landing page

---
+++ MovieBioscope/app/src/main/java/com/hyperbound/moviebioscope/ui/MainActivity.java
@@ -0,0 +1,36 @@
+package com.hyperbound.moviebioscope.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.hyperbound.moviebioscope.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fullScreen();
+        addLayout();
+    }
+
+    private void addLayout() {
+        setContentView(R.layout.main_layout);
+        FragmentTransaction lTransaction = getSupportFragmentManager().beginTransaction();
+        lTransaction.add(R.id.container, Landingfragment.newInstance());
+        lTransaction.commit();
+    }
+
+
+    private void fullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+}

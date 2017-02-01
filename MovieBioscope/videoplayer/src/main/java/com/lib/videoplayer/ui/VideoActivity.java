c4f7b7be4071f970ca95e821f06ea4c2dda5c177
Aron <aron.raj35@gmail.com>
2017-02-01 23:21:45 +0530

    created video player ui

--- MovieBioscope/videoplayer/src/main/java/com/lib/videoplayer/ui/VideoActivity.java
+++ MovieBioscope/videoplayer/src/main/java/com/lib/videoplayer/ui/VideoActivity.java
@@ -3,38 +3,50 @@ package com.lib.videoplayer.ui;
 import android.content.ContentValues;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.lib.videoplayer.R;
 import com.lib.videoplayer.database.VideoDatabaseProvider;
 
-public class VideoActivity extends AppCompatActivity {
+import java.io.File;
+
+public class VideoActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private VideoView mMovieView;
     private VideoView mAdvView;
     private int videoTime;
+    private RelativeLayout mHeader;
+    private RelativeLayout mFooter;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fullScreen();
         setContentView(R.layout.video_layout);
-        putDummyData();
+        mHeader = (RelativeLayout) findViewById(R.id.top_banner_video);
+        mFooter = (RelativeLayout) findViewById(R.id.bottom_banner_video);
         mMovieView = (VideoView) findViewById(R.id.movie_view);
         mAdvView = (VideoView) findViewById(R.id.ad_video);
-        mMovieView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample));
-        mMovieView.setMediaController(new MediaController(this));
+        mMovieView.setVideoURI(Uri.parse(Environment.getExternalStorageDirectory() + "/movie_bioscope/Spotlight.mp4"));
+        mMovieView.setMediaController(null);
         mMovieView.requestFocus();
         mMovieView.start();
-
-        mAdvView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.ad));
-        mAdvView.setMediaController(new MediaController(this));
+        mMovieView.setOnTouchListener(this);
+        mHandler = new Handler();
+        mAdvView.setVideoURI(Uri.parse(Environment.getExternalStorageDirectory() + "/movie_bioscope/ad1.mp4"));
+        mAdvView.setMediaController(null);
 
         mMovieView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -59,19 +71,50 @@ public class VideoActivity extends AppCompatActivity {
                 mMovieView.seekTo(videoTime);
             }
         });
-
-
     }
 
-    private void fullScreen() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     private void putDummyData() {
         ContentValues lValues = new ContentValues();
         lValues.put(VideoDatabaseProvider.VIDEO_COLUMNS.NAME, "hello");
         getContentResolver().insert(VideoDatabaseProvider.CONTENT_URI_VIDEO_TABLE, lValues);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                showHeaderFooter();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mHeader.setVisibility(View.GONE);
+                        mFooter.setVisibility(View.GONE);
+                    }
+                }, 3 * 1000);
+                break;
+
+        }
+        return false;
+    }
+
+    private void showHeaderFooter(){
+        mHeader.setVisibility(View.VISIBLE);
+        mFooter.setVisibility(View.VISIBLE);
+        mHeader.bringToFront();
+        mFooter.bringToFront();
     }
 }
